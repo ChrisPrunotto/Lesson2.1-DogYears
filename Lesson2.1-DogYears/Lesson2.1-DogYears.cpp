21c9a63bcf73f1f11ebbbdaca776767f210d3911
Chris Prunotto <Chris.Prunotto@gmail.com>
2020-07-27 16:05:14 -0500

    Committing Project From Codecademy.

--- Lesson2.1-DogYears/Lesson2.1-DogYears.cpp
+++ Lesson2.1-DogYears/Lesson2.1-DogYears.cpp
@@ -1,20 +1,31 @@
-// Lesson2.1-DogYears.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Lesson1.3-DogYears.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+#include <string>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int dog_age;
+	std::string dog_name;
+
+	//first two years are 21 years.
+	int early_years = 21;
+	//each following year is 4 human years.
+	int later_years;
+	int human_years;
+
+	std::cout << "What is the dog's name? ";
+	std::cin >> dog_name;
+
+	std::cout << "How old is the dog? ";
+	std::cin >> dog_age;
+
+	//later years are 4 years each.
+	later_years = (dog_age - 2) * 4;
+
+	//early years + later years = human equivalant.
+	human_years = early_years + later_years;
+
+	std::cout << "My name is " << dog_name << "! I am " << human_years << " old. \n";
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
